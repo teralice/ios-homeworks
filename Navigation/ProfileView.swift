d4d723c842823b3677d02a52db8439d6f2133823
alisa <alisa@MacBook-Pro-Ivan-2.local>
2022-02-25 14:32:13 +0300

    fix storyboard

--- Navigation/ProfileView.swift
+++ Navigation/ProfileView.swift
@@ -13,4 +13,24 @@ class ProfileView: UIView {
     @IBOutlet weak var dateLabel: UILabel!
     @IBOutlet weak var placeLabel: UILabel!
     @IBOutlet weak var textView: UITextView!
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        self.setupView()
+    }
+    
+    private func setupView() {
+        let view = self.loadViewFromXib()
+        self.addSubview(view)
+    }
+
+    private func loadViewFromXib() -> UIView {
+        guard let view = Bundle.main.loadNibNamed("ProfileView", owner: self, options: nil)?.first as? UIView else { return UIView() }
+        
+        return view
+    }
 }
--- Navigation/ProfileViewController.swift
+++ Navigation/ProfileViewController.swift
@@ -11,8 +11,5 @@ class ProfileViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        if let subview = Bundle.main.loadNibNamed("ProfileView", owner: nil, options: nil)?.first as? UIView {
-            self.view.addSubview(subview)
-        }
     }
 }
