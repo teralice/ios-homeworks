569f4987365e2e5cdba791453e9538427f49e88d
alisa <alisa@MacBook-Pro-Ivan-2.local>
2022-03-02 13:58:46 +0300

    start

---
+++ Navigation/Profile/LogInView.swift
@@ -0,0 +1,20 @@
+//
+//  LogInView.swift
+//  Navigation
+//
+//  Created by alisa on 02.03.2022.
+//
+
+import UIKit
+
+class LogInView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
